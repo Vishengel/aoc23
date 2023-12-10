a32ef86cc60ef0e5ba9e4e222ac2a22d0b06cb27
Vishengel <jellevisser1994@hotmail.com>
2023-12-10 01:11:34 +0100

    Skeletons for 8-25

---
+++ 8/8.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+from typing import List
+
+from util import load_txt_file_as_list_of_str, SubProblem
+
+INPUT_FILE_PATH = Path("input")
+SUB_PROBLEM = SubProblem.ONE
+        
