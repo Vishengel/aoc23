6aded75c59f980839c08e3d3f765f45a4053a638
Jelle <jelle@web-iq.com>
2023-12-21 11:10:58 +0100

    Start of p10

--- p10/10.py
+++ p10/10.py
@@ -1,8 +1,22 @@
+from enum import Enum
 from pathlib import Path
 
-from util import load_txt_file_as_list_of_str, SubProblem
+from util import SubProblem, load_txt_file_as_str
 
-INPUT_FILE_PATH = Path("input")
+INPUT_FILE_PATH = Path("input_test_1")
 SUB_PROBLEM = SubProblem.ONE
 
-input_array = load_txt_file_as_list_of_str(INPUT_FILE_PATH)
+
+class Direction(Enum):
+    NORTH = "north"
+    EAST = "east"
+    SOUTH = "south"
+    WEST = "west"
+
+
+SYMBOL_TO_PIPE_MAP = {"|": (Direction.NORTH, Direction.SOUTH), "-": (Direction.EAST, Direction.WEST),
+                      "L": (Direction.NORTH, Direction.EAST), "J": (Direction.NORTH, Direction.WEST),
+                      "7": (Direction.SOUTH, Direction.WEST), "F": (Direction.SOUTH, Direction.EAST)}
+
+input_array = load_txt_file_as_str(INPUT_FILE_PATH)
+print(input_array)
