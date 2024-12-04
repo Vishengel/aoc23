60a595340078248eb40918050b72fa64ee55ad94
Jelle <jelle@web-iq.com>
2024-12-04 10:53:29 +0100

    Solution for puzzle1

--- 2024/puzzle1/puzzle1.py
+++ 2024/puzzle1/puzzle1.py
@@ -2,7 +2,35 @@ from pathlib import Path
 
 from util import load_txt_file_as_list_of_str, SubProblem
 
-INPUT_FILE_PATH = Path("input")
+INPUT_FILE_PATH = Path("input1.txt")
 SUB_PROBLEM = SubProblem.ONE
 
-input_array = load_txt_file_as_list_of_str(INPUT_FILE_PATH)
+
+def split_list_and_sort(input_array):
+    list0 = []
+    list1 = []
+    for line in input_array:
+        digits = line.split("  ")
+        list0.append(int(digits[0]))
+        list1.append(int(digits[1]))
+    return sorted(list0), sorted(list1)
+
+
+def get_dist(list0, list1) -> int:
+    total_distance = 0
+    for (digit0, digit1) in zip(list0, list1):
+        total_distance += abs(digit0 - digit1)
+    return total_distance
+
+def main():
+    input_array = load_txt_file_as_list_of_str(INPUT_FILE_PATH)
+    # print(input_array)
+    list0, list1 = split_list_and_sort(input_array)
+    # print(list0)
+    # print(list1)
+    total_distance = get_dist(list0, list1)
+    print(total_distance)
+
+
+if __name__ == "__main__":
+    main()
--- init_puzzle.py
+++ init_puzzle.py
@@ -2,13 +2,8 @@ import os
 import pathlib
 from pathlib import Path
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 YEAR = 2024
 DAY = 1
-SESSION_COOKIE = os.getenv("AOC_SESSION")
 
 
 def create_dir() -> Path:
@@ -19,18 +14,27 @@ def create_dir() -> Path:
     return puzzle_dir
 
 
-def create_skeleton_script(path):
+def create_skeleton_script(puzzle_dir):
     skeleton = """from pathlib import Path
 
 from util import load_txt_file_as_list_of_str, SubProblem
 
-INPUT_FILE_PATH = Path("input")
+INPUT_FILE_PATH = Path("input0.txt")
 SUB_PROBLEM = SubProblem.ONE
 
-input_array = load_txt_file_as_list_of_str(INPUT_FILE_PATH)
-"""
+def main():
+    input_array = load_txt_file_as_list_of_str(INPUT_FILE_PATH)
 
-    with open(os.path.join(path, f"puzzle{DAY}.py"), 'w') as skeleton_file:
+if __name__ == "__main__":
+    main()
+"""
+    path = (puzzle_dir / f"puzzle{DAY}.py")
+
+    if path.exists():
+        print(f"File puzzle{DAY}.py already exists in {puzzle_dir}")
+        return
+
+    with open(path, 'w') as skeleton_file:
         skeleton_file.write(skeleton)
 
 
@@ -40,6 +44,12 @@ def get_input():
 
 def create_files(puzzle_dir: Path):
     create_skeleton_script(puzzle_dir)
+    for idx in range(3):
+        try:
+            (puzzle_dir / f"input{idx}.txt").touch(exist_ok=False)
+        except FileExistsError:
+            print(f"File input{idx}.txt already exists in {puzzle_dir}")
+            continue
 
 
 def main():
