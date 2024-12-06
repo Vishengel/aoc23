480379ab07ca96172647cfcc36831622a8dfce7a
Jelle <jellevisser1994@hotmail.com>
2024-12-06 16:05:00 +0100

    Solution to Day 1, remove create_dirs script

--- 2024/day1/puzzle1.py
+++ 2024/day1/puzzle1.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 from util import load_txt_file_as_list_of_str, SubProblem
 
-INPUT_FILE_PATH = Path("input1.txt")
-SUB_PROBLEM = SubProblem.ONE
+INPUT_FILE_PATH = Path("input3.txt")
+SUB_PROBLEM = SubProblem.TWO
 
 
 def split_list_and_sort(input_array):
@@ -22,15 +22,36 @@ def get_dist(list0, list1) -> int:
         total_distance += abs(digit0 - digit1)
     return total_distance
 
+def get_similarity_score(list0, list1) -> int:
+    transposition_table = {}
+    similarity_score = 0
+
+    for digit0 in list0:
+        if digit0 in transposition_table:
+            similarity_score += transposition_table[digit0]
+            continue
+
+        count = 0
+        for digit1 in list1:
+            if digit1 == digit0:
+                count += 1
+
+        score = digit0*count
+        transposition_table[digit0] = score
+        similarity_score += score
+
+    return similarity_score
+
+
 def main():
     input_array = load_txt_file_as_list_of_str(INPUT_FILE_PATH)
-    # print(input_array)
     list0, list1 = split_list_and_sort(input_array)
-    # print(list0)
-    # print(list1)
-    total_distance = get_dist(list0, list1)
-    print(total_distance)
-
+    if SUB_PROBLEM == SubProblem.ONE:
+        total_distance = get_dist(list0, list1)
+        print(total_distance)
+    else:
+        similarity_score = get_similarity_score(list0, list1)
+        print(similarity_score)
 
 if __name__ == "__main__":
     main()
