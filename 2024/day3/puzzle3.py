236f77f989bf6eca2a09b93c43188587a24ea00f
Vishengel <jellevisser1994@hotmail.com>
2024-12-12 18:56:17 +0100

    Solution to 3b

--- 2024/day3/puzzle3.py
+++ 2024/day3/puzzle3.py
@@ -8,8 +8,7 @@ INPUT_FILE_PATH = Path("input1.txt")
 SUB_PROBLEM = SubProblem.TWO
 
 VALID_MUL_REGEX = r"mul\({1}(\d{1,3}),{1}(\d{1,3})\){1}"
-DO_REGEX = r'\bdo\b(?!n\'t)'
-DONT_DO_LAZY_REGEX = r"(don't\(\).*)(?:do\(\)+?)"
+
 
 def get_digits_from_valid_mul_patterns(instruction_set: str) -> List[str]:
     return re.findall(VALID_MUL_REGEX, instruction_set)
@@ -19,31 +18,25 @@ def execute_mul_instruction(instruction: Tuple[str, str]) -> int:
     return int(instruction[0]) * int(instruction[1])
 
 
-def filter_disables(instruction_set: str, enable: bool = True) -> str:
-    split_token = "don't()" if enable else "do()"
-    split_instruction_set = instruction_set.split(split_token) # if enable else re.split(DO_REGEX, instruction_set)
+def filter_negated_instructions(instruction_set: str) -> str:
+    split_instruction_set = instruction_set.split("don't()")
+    filtered_instruction_set = split_instruction_set[0]
 
-    if len(split_instruction_set) == 1 and enable:
-        return ""
+    for idx, block in enumerate(split_instruction_set[1:]):
+        split_block = block.split("do()")
+        if len(split_block) == 1:
+            # Block does not contain a do(), so it should not be executed
+            continue
+        filtered_instruction_set += "".join(split_block[1:])
 
-    enable_blocks = split_instruction_set[0::2] if enable else split_instruction_set[1::2]
-    disable_blocks = split_instruction_set[1::2] if enable else split_instruction_set[0::2]
-
-    for block in disable_blocks:
-        filtered_string = filter_disables(block, not enable)
-        enable_blocks.append(filtered_string)
-
-    return "".join(enable_blocks)
+    return filtered_instruction_set
 
 
 def main():
     input_string = load_txt_file_as_str(INPUT_FILE_PATH)
-    print(input_string)
 
     if SUB_PROBLEM == SubProblem.TWO:
-        # input_string = filter_disables(input_string)
-        input_string = re.sub(DONT_DO_LAZY_REGEX, "do()", input_string)
-        print(input_string)
+        input_string = filter_negated_instructions(input_string)
 
     valid_mul_patterns = get_digits_from_valid_mul_patterns(input_string)
     print(valid_mul_patterns)
