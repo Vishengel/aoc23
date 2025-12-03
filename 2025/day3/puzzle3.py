6d994c76fe6ac15c8d8688fb9076d459c110a301
Vishengel <jellevisser1994@hotmail.com>
2025-12-03 20:20:19 +0100

    WIP gnngngng

--- 2025/day3/puzzle3.py
+++ 2025/day3/puzzle3.py
@@ -5,8 +5,21 @@ from util import load_txt_file_as_list_of_str, SubProblem
 INPUT_FILE_PATH = Path("input0.txt")
 SUB_PROBLEM = SubProblem.ONE
 
+def find_highest_joltage(bank: str, n_batteries=2):
+    highest_joltage = 0
+    for idx, battery in enumerate(bank[:-(n_batteries-1)]):
+        # highest_next = max([int(next_bat) for next_bat in bank[idx+1:]])
+        highest_next = sorted([int(next_bat) for next_bat in bank[idx+1:]], reverse=True)[:n_batteries-1]
+        highest_next_strin = "".join(str(val) for val in highest_next)
+        joltage = int(battery + highest_next_strin)
+        highest_joltage = joltage if joltage > highest_joltage else highest_joltage
+    return highest_joltage
+
 def main():
     input_array = load_txt_file_as_list_of_str(INPUT_FILE_PATH)
+    highest_joltages = [find_highest_joltage(bank, n_batteries=12) for bank in input_array]
+    print(highest_joltages)
+    print(sum(highest_joltages))
 
 if __name__ == "__main__":
     main()
